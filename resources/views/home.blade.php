25c03bdcd90439ddc78cee0fda2f6e194e9f4c46
jfbaeza <paco82009@gmail.com>
2024-11-01 14:50:14 -0600

    Navbar y Carrusel de imagenes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,63 @@
 @section('title', 'Home')
 
 @section('body')
-    <div>
-        <h1>Hola</h1>
+
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{{ asset('images/About.webp') }}" class="d-block w-100" alt="..."
+                    style="max-height: 400px; object-fit: cover;">
+                <div class="carousel-caption d-none d-md-block custom-caption">
+                    <h5>About me</h5>
+                    <p>Get to know a bit about me and my experience as a developer.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('images/Tech.webp') }}" class="d-block w-100" alt="..."
+                    style="max-height: 400px; object-fit: cover;">
+                <div class="carousel-caption d-none d-md-block custom-caption">
+                    <h5>Tech & Skill</h5>
+                    <p>Some of the technologies I currently work with.</p>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="{{ asset('images/Contact.webp') }}" class="d-block w-100" alt="..."
+                    style="max-height: 400px; object-fit: cover;">
+                <div class="carousel-caption d-none d-md-block custom-caption">
+                    <h5>Connect with Us</h5>
+                    <p>Feel free to reach out to me if you have any questions or need assistance.</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
+
+<div class="container-flex bg-dark text-white text-center">
+    <br>
+    <h3><strong>About me</strong></h3>
+    <div class="row">
+        <div class="col-md-8">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse modi ex quisquam nulla temporibus veritatis sed atque, pariatur quibusdam ducimus fuga in porro eaque impedit, quos deleniti accusantium odit sapiente.</p>
+        </div>
+    </div>
+</div>
+
+
 @endsection
 
 @section('scripts')
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,16 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
     @yield('styles')
     <title>@yield('title')</title>
 </head>
-<body>
-    @yield('body')
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{ route('home') }}">
+                    <img src="{{ asset('images/Logo.png') }}" alt="" width="30" height="24"
+                        class="d-inline-block align-text-top">
+                    My professional legacy
+                </a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">About me</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Tech & Skill</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Connect with Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        @yield('body')
+    </main>
+
+    <footer class="footer mt-auto py-3 bg-light">
+        <div class="container-fluid">
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
     @yield('scripts')
 </body>
+
 </html>
